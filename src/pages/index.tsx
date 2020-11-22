3fc4dd2ea2eeeb4449ce0d26e96eaf3820825ce1
osmarpetry <osmarpetry@gmail.com>
2020-11-22 03:40:10 -0300

    styled-components global style and images

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,12 @@
-import Head from 'next/head'
-
-import FullpageInformation from '../components/FullpageInformation'
+import FullpageInformation from 'components/FullpageInformation'
 
 export default function Home() {
   return (
-    <div>
-      <Head>
-        <title>Yan Store</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
+    <>
       <FullpageInformation
         title="Webside in works"
         description="Come back later!"
       />
-    </div>
+    </>
   )
 }
