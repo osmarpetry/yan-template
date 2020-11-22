c22ea84d0ca7b56e5e097caebfaafb3e28c9ef59
osmarpetry <osmarpetry@gmail.com>
2020-11-22 04:11:55 -0300

    storybook with the first component

--- src/components/FullpageInformation/index.tsx
+++ src/components/FullpageInformation/index.tsx
@@ -1,23 +1,21 @@
 import * as S from './styled'
 
+export interface FullpageInformationProps {
+  title: string
+  description: string
+  imgSrc?: string
+  imgAlt?: string
+}
+
 export default function FullpageInformation({
   title,
   description,
-  img = {
-    src: '/img/hero-illustration.svg',
-    alt: 'Um desenvolvedor de frente para uma tela com código.'
-  }
-}: {
-  title: string
-  description: string
-  img?: {
-    src: string
-    alt: string
-  }
-}) {
+  imgSrc = '/img/hero-illustration.svg',
+  imgAlt = 'Um desenvolvedor de frente para uma tela com código.'
+}: FullpageInformationProps) {
   return (
     <S.Wrapper>
-      <S.Illustration src={img.src} alt={img.alt} />
+      <S.Illustration src={imgSrc} alt={imgAlt} />
       <S.Title>{title}</S.Title>
       <S.Description>{description}</S.Description>
     </S.Wrapper>
