8a274702272ef78e89082074af5c7c5c87b3d634
osmarpetry <osmarpetry@gmail.com>
2020-11-22 03:53:57 -0300

    first component with test, with styled-component

--- src/components/FullpageInformation/index.tsx
+++ src/components/FullpageInformation/index.tsx
@@ -1,14 +1,25 @@
+import * as S from './styled'
+
 export default function FullpageInformation({
   title,
-  description
+  description,
+  img = {
+    src: '/img/hero-illustration.svg',
+    alt: 'Um desenvolvedor de frente para uma tela com código.'
+  }
 }: {
   title: string
   description: string
+  img?: {
+    src: string
+    alt: string
+  }
 }) {
   return (
-    <main>
-      <h1>{title}</h1>
-      <p>{description}</p>
-    </main>
+    <S.Wrapper>
+      <S.Illustration src={img.src} alt={img.alt} />
+      <S.Title>{title}</S.Title>
+      <S.Description>{description}</S.Description>
+    </S.Wrapper>
   )
 }
---
+++ src/components/FullpageInformation/styled.ts
@@ -0,0 +1,28 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.main`
+  background-color: #444877;
+  color: #fff;
+  width: 100%;
+  height: 100%;
+  padding: 3rem;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
+export const Title = styled.h1`
+  margin-top: 1.5rem;
+  font-size: 4.5rem;
+`
+
+export const Description = styled.p`
+  font-size: 3rem;
+  font-weight: 400;
+`
+
+export const Illustration = styled.img`
+  margin-top: 3rem;
+  width: min(30rem, 100%);
+`
