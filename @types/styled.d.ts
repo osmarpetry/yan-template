ba888fb8802d08393bfa38e1d96d78ac4d854bb6
osmarpetry@gmail.com <osmarpetry@gmail.com>
2021-11-18 18:21:56 -0300

    update packages

---
+++ @types/styled.d.ts
@@ -0,0 +1,78 @@
+// import original module declarations
+import 'styled-components'
+
+// and extend them!
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    grid: Grid
+    border: Border
+    font: Font
+    colors: Colors
+    spacings: Spacings
+    layers: Layers
+    transition: Transition
+  }
+}
+
+export interface Grid {
+  container: string
+  gutter: string
+}
+
+export interface Border {
+  radius: string
+}
+
+export interface Sizes {
+  xsmall: string
+  small: string
+  medium: string
+  large: string
+  xlarge: string
+  xxlarge: string
+  huge: string
+}
+
+export interface Font {
+  family: string
+  light: number
+  normal: number
+  bold: number
+  sizes: Sizes
+}
+
+export interface Colors {
+  primary: string
+  secondary: string
+  mainBg: string
+  lightBg: string
+  white: string
+  black: string
+  lightGray: string
+  gray: string
+  darkGray: string
+  red: string
+}
+
+export interface Spacings {
+  xxsmall: string
+  xsmall: string
+  small: string
+  medium: string
+  large: string
+  xlarge: string
+  xxlarge: string
+}
+
+export interface Layers {
+  base: number
+  menu: number
+  overlay: number
+  modal: number
+  alwaysOnTop: number
+}
+
+export interface Transition {
+  default: string
+  fast: string
+}
